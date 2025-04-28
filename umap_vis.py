18c14f4d6f717001a0f80528080d78d683878ea8
Anthony Kang <kangarooster789@gmail.com>
2025-04-28 15:48:00 -0400

    added more customizable fields

--- umap_vis.py
+++ umap_vis.py
@@ -3,9 +3,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.colors as mcolors
 
-path_to_embeddings = '/Users/anthony/Documents/2024-2025 Brown Stuff/Spring 2025/DL_in_Genomics/sc-DMAE/data/embedding_59.npy'
-path_to_labels = '/Users/anthony/Documents/2024-2025 Brown Stuff/Spring 2025/DL_in_Genomics/sc-DMAE/data/types_59.txt'
+#Change this to the embeddings (.npy file)
+path_to_embeddings = '/Users/anthony/Documents/2024-2025 Brown Stuff/Spring 2025/DL_in_Genomics/data/embedding_59.npy'
 
+#Change this to the labels (.txt file)
+path_to_labels = '/Users/anthony/Documents/2024-2025 Brown Stuff/Spring 2025/DL_in_Genomics/data/types_59.txt'
+
+#Change these to the desired label names. Ensure that you have as many names as you have unique labels
+label_names = np.array(['label 1', 'label 2', 'label 3', 'label 4', 'label 5', 'label 6', 'label 7', 'label 8'])
+
+#Change to desired plot title
+plot_title = 'UMAP Visualization of Hrvatin 59'
+
+#Load embeddings + labels.
 embeddings = np.load(path_to_embeddings)
 labels = np.genfromtxt(path_to_labels, delimiter=',', skip_header=1)[:, 1]
 
@@ -31,12 +41,12 @@ for i, label in enumerate(unique_labels):
         embedding_2d[idx, 0],   # x values
         embedding_2d[idx, 1],   # y values
         color=colors_list[i],   # assign color
-        label=str(label),       # label for the legend
+        label=str(label_names[i]),       # label for the legend
         s=2                    # size of the points
     )
 
 # Add legend
 plt.legend(markerscale=2, bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
-plt.title('UMAP Visualization of Hrvatin 59')
+plt.title(plot_title)
 plt.tight_layout()
 plt.show()
